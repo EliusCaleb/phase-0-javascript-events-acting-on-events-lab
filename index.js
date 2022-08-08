7d79a8753f41073bb9c616349d3ef502cc39e824
ELIUS <eliuswanyoro@gmail.com>
2022-08-08 11:06:05 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -14,3 +14,15 @@ document.addEventListener("keydown", function (e) {
 });
 
 
+function moveDodgerRight() {
+    var leftNumbers = dodger.style.left;
+    var left = parseInt(leftNumbers, 10);
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
